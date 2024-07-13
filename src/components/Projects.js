7c53d8c5353b999951161b3b1dea1db5371becc4
Praveenraj Pattar <pattar.praveenraj3537@gmail.com>
2024-07-13 14:24:01 +0530

    added the HumanPoseEstimation details

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,20 +8,20 @@ const Projects = () => {
     const [projects, setProjects] = useState(data_projects)
 
     const handlFilterCategory = (name) => {
-       const array =  data_projects.filter(project => project.category.includes(name))
-       setProjects(array)
-    } 
+        const array = data_projects.filter(project => project.category.includes(name))
+        setProjects(array)
+    }
 
     const project_motion = {
-        hidden:{
-            y:'-100vh'
+        hidden: {
+            y: '-100vh'
         },
-        visible:{
-            y:0,
-            transition:{
-                delay:0.5,
-                duration:0.7,
-                type:'spring'
+        visible: {
+            y: 0,
+            transition: {
+                delay: 0.5,
+                duration: 0.7,
+                type: 'spring'
             }
         }
     }
@@ -34,18 +34,19 @@ const Projects = () => {
         >
             <div className="projects_navbar">
                 <div onClick={() => setProjects(data_projects)}>All</div>
+                <div onClick={() => handlFilterCategory('python')}>Python</div>
+                <div onClick={() => handlFilterCategory('ML/DL')}>ML/DL</div>
                 <div onClick={() => handlFilterCategory('react')}>React</div>
                 <div onClick={() => handlFilterCategory('angular')}>Angular</div>
-                <div onClick={() => handlFilterCategory('python')}>Python</div>
                 <div onClick={() => handlFilterCategory('c')}>C</div>
                 <div onClick={() => handlFilterCategory('nestjs')}>NestJS</div>
 
             </div>
             <div className="row">
                 {
-                    projects.map(project=>
+                    projects.map(project =>
                         <ProjectDetails key={project.name} project={project} />
-                        )
+                    )
                 }
             </div>
         </motion.div>
